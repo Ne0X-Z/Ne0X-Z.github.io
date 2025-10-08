c63333ccd630161163995d7fc714a1e1f97adf31
ZCHAlann <jerala2035@gmail.com>
2025-10-07 22:38:36 -0500

    fixing and updating

--- js/content-loader.js
+++ js/content-loader.js
@@ -1,9 +1,6 @@
-// js/content-loader.js
-
-// Configuración
 const JSON_PATH = 'files/json/lastest.json';
 
-// Mapeo de íconos SVG
+
 const ICONS = {
     box: `<path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line>`,
 
